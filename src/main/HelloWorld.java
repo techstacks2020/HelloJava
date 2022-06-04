316bb272d966ca7687b6878f96f73ee335900b4a
techstacks2020 <techstacks2020@gmail.com>
2022-06-04 22:13:38 +0530

    Added new line

--- src/main/HelloWorld.java
+++ src/main/HelloWorld.java
@@ -3,5 +3,6 @@ package main;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World !!");
+        System.out.println("Added new line !!");
     }
 }
